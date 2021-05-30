1f9bb056ad1f0d1c74f7d45a56f6bb3bd84d7055
Ashkan Gholizadeh <ashkan.ghk84@gmail.com>
2021-05-30 13:01:15 +0100

    initial project

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            {/*Top nav */}
+            <div>
+                
+            </div>
+
+            {/*Top nav */}
+
+            <div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
