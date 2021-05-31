84a74f5c816c2e51b174ed3098c2de113d55f942
Ashkan Gholizadeh <ashkan.ghk84@gmail.com>
2021-05-31 11:33:19 +0100

    authentication

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxtwn6LFnXYeqSOw6_64OCwXIMZvqyKQU",
+    authDomain: "gmygameshop.firebaseapp.com",
+    projectId: "gmygameshop",
+    storageBucket: "gmygameshop.appspot.com",
+    messagingSenderId: "223254405589",
+    appId: "1:223254405589:web:ccf7c9f24adcc3d9fe1d5c",
+    measurementId: "G-C86CHFDGPZ"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,15 @@ import {
     SearchIcon,
     ShoppingCartIcon,
   } from "@heroicons/react/outline";
+  import { signIn, signOut, useSession } from 'next-auth/client';
 
 function Header() {
+
+    const [session] = useSession();
+
+
+    
+
     return (
         <header>
             {/*Top nav */}
@@ -27,17 +34,19 @@ function Header() {
 
                 {/* Right */}
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-                    <div className='link' >
-                        <p>Hello Ash</p>
+                    <div onClick={!session ? signIn : signOut} className='cursor-pointer link' >
+                        <p className='hover:underline'>
+                            {session ? `Hello, ${session.user.name}` : 'Sign In'}
+                            </p>
                         <p className='font-extrabold md:text-sm'>Account & List</p>
                     </div>
 
-                    <div className='link'>
+                    <div className='cursor-pointer link'>
                         <p>Returns</p>
                         <p className='font-extrabold md:text-sm'>& Orders</p>
                     </div>
 
-                    <div className='relative link flex items-center'>
+                    <div className='relative flex items-center cursor-pointer link'>
                         <span className=' absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
                         <ShoppingCartIcon className='h-10' />
                         <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
